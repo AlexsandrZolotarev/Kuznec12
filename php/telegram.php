39fc98b3a3d79ab977ee89c911d6bb981b72c100
Sasha <38#mail.ru>
2024-10-21 19:39:44 +0300

    LandingPageKuznec12

---
+++ php/telegram.php
@@ -0,0 +1,26 @@
+<?php
+
+/* https://api.telegram.org/bot5732536272:AAH8WoSUSj3jcoupKLdV50-AphjdUDsT4G0/getUpdates,
+где, XXXXXXXXXXXXXXXXXXXXXXX - токен вашего бота, полученный ранее */
+
+$name = $_POST['nick'];
+$phone = $_POST['phone'];
+$token = "5732536272:AAH8WoSUSj3jcoupKLdV50-AphjdUDsT4G0";
+$chat_id = "-602599820";
+$arr = array(
+  'Имя пользователя: ' => $name,
+  'Телефон: ' => $phone,
+);
+
+foreach($arr as $key => $value) {
+  $txt .= "<b>".$key."</b> ".$value."%0A";
+};
+
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+if ($sendToTelegram) {
+  header('Location: thank-you.html');
+} else {
+  echo "Error";
+}
+?>
